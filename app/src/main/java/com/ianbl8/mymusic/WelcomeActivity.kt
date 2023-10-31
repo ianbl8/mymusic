3d117ec2048c5e5612253b91ea793d4c1c66a85d
Ian Blake <ian@ianbl8.com>
2023-10-31 23:15:01 +0000

    Click start button to go from WelcomeActivity to ListActivity

--- app/src/main/java/com/ianbl8/mymusic/WelcomeActivity.kt
+++ app/src/main/java/com/ianbl8/mymusic/WelcomeActivity.kt
@@ -1,11 +1,22 @@
 package com.ianbl8.mymusic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var btnStart: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        btnStart = findViewById(R.id.btnStart)
+        btnStart.setOnClickListener {
+            val intent = Intent(this@WelcomeActivity, ListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
