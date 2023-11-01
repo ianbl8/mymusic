5950fc020fb05af5a453c6af522ac8d14c90a4f8
Ian Blake <ian@ianbl8.com>
2023-11-01 22:52:15 +0000

    Implement RecyclerView and CardView for ListActivity

--- app/src/main/java/com/ianbl8/mymusic/activities/ListActivity.kt
+++ app/src/main/java/com/ianbl8/mymusic/activities/ListActivity.kt
@@ -1,25 +1,57 @@
 package com.ianbl8.mymusic.activities
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import com.ianbl8.mymusic.R
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ianbl8.mymusic.databinding.ActivityListBinding
+import com.ianbl8.mymusic.databinding.CardItemBinding
+import com.ianbl8.mymusic.main.MainApp
+import com.ianbl8.mymusic.models.ItemModel
 import timber.log.Timber.Forest.i
 
 class ListActivity : AppCompatActivity() {
 
-    private lateinit var btnStart: Button
+    lateinit var app: MainApp
+    private lateinit var binding: ActivityListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list)
-
+        binding = ActivityListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         i("ListActivity started")
 
-        btnStart = findViewById(R.id.btnStart)
-        btnStart.setOnClickListener {
-            startActivity(Intent(this@ListActivity, ItemActivity::class.java))
+        app = application as MainApp
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = ItemAdapter(app.items)
+    }
+}
+
+class ItemAdapter constructor(private var items: List<ItemModel>) : RecyclerView.Adapter<ItemAdapter.MainHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        val binding = CardItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MainHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val item = items[holder.adapterPosition]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class MainHolder(private val binding: CardItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SetTextI18n")
+        fun bind(item: ItemModel) {
+            binding.itemTitle.text = item.title
+            binding.itemArtist.text = item.artist
+            binding.itemYear.text = "(${item.year})"
         }
     }
 }
--- app/src/main/java/com/ianbl8/mymusic/main/MainApp.kt
+++ app/src/main/java/com/ianbl8/mymusic/main/MainApp.kt
@@ -13,5 +13,18 @@ class MainApp: Application() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         i("mymusic started")
+
+        /* Items for testing
+        items.add(ItemModel("Test0","Ring Ring", "ABBA", "1973", true, true))
+        items.add(ItemModel("Test1","Waterloo", "ABBA", "1974", true, true))
+        items.add(ItemModel("Test2","ABBA", "ABBA", "1975", true, true))
+        items.add(ItemModel("Test3","Arrival", "ABBA", "1976", true, true))
+        items.add(ItemModel("Test4","The Album", "ABBA", "1977", true, true))
+        items.add(ItemModel("Test5","Voulez-Vous", "ABBA", "1979", true, true))
+        items.add(ItemModel("Test6","Super Trouper", "ABBA", "1980", true, true))
+        items.add(ItemModel("Test7","The Visitors", "ABBA", "1981", true, true))
+        items.add(ItemModel("Test8","ABBA Live", "ABBA", "1986", true, true))
+        items.add(ItemModel("Test9","Voyage", "ABBA", "2021", true, true))
+         */
     }
 }
