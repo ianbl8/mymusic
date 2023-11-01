f9389100492b1324e52d68f213a6e99d8eaaee81
Ian Blake <ian@ianbl8.com>
2023-11-01 15:14:11 +0000

    Logging implemented using Timber

--- app/src/main/java/com/ianbl8/mymusic/ListActivity.kt
+++ app/src/main/java/com/ianbl8/mymusic/ListActivity.kt
@@ -2,10 +2,15 @@ package com.ianbl8.mymusic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.Forest.i
 
 class ListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        Timber.plant(Timber.DebugTree())
+        i("ListActivity started")
     }
 }
--- app/src/main/java/com/ianbl8/mymusic/WelcomeActivity.kt
+++ app/src/main/java/com/ianbl8/mymusic/WelcomeActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import timber.log.Timber
+import timber.log.Timber.Forest.i
 
 class WelcomeActivity : AppCompatActivity() {
 
@@ -12,6 +14,9 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        Timber.plant(Timber.DebugTree())
+        i("WelcomeActivity started")
 
         btnStart = findViewById(R.id.btnStart)
         btnStart.setOnClickListener {
