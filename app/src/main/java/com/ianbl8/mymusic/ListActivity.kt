a3220a8ff029c93eb3b8085dbbca0452999c25d8
Ian Blake <ian@ianbl8.com>
2023-11-01 21:14:57 +0000

    Initial ItemActivity with input

--- app/src/main/java/com/ianbl8/mymusic/ListActivity.kt
+++ app/src/main/java/com/ianbl8/mymusic/ListActivity.kt
@@ -1,16 +1,23 @@
 package com.ianbl8.mymusic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import timber.log.Timber
+import android.widget.Button
 import timber.log.Timber.Forest.i
 
 class ListActivity : AppCompatActivity() {
+
+    private lateinit var btnStart: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
-        Timber.plant(Timber.DebugTree())
         i("ListActivity started")
-    }
+
+        btnStart = findViewById(R.id.btnStart)
+        btnStart.setOnClickListener {
+            startActivity(Intent(this@ListActivity, ItemActivity::class.java))
+        }    }
 }
