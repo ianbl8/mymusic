7900966254495ead2fbf55cc8647f7b94cbff6eb
Ian Blake <ian@ianbl8.com>
2023-11-05 17:32:11 +0000

    Version updated to v0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ianbl8.mymusic"
         minSdk = 31
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
         vectorDrawables.useSupportLibrary = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
