d029d3b2a660ee4d783adf7dc402fdc4d94d337c
Ian Blake <ian@ianbl8.com>
2023-11-05 11:52:34 +0000

    Version number updated to match tag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ianbl8.mymusic"
         minSdk = 31
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2.0"
         vectorDrawables.useSupportLibrary = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
