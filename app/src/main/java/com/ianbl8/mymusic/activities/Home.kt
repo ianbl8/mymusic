95256822ee5d51597a48ba9b6cc927725c77a621
Ian Blake <ian@ianbl8.com>
2023-12-25 13:25:23 +0000

    Introduce navigation and fragments

---
+++ app/src/main/java/com/ianbl8/mymusic/activities/Home.kt
@@ -0,0 +1,12 @@
+package com.ianbl8.mymusic.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ianbl8.mymusic.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+    }
+}
