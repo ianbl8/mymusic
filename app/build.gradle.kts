d66ffcd005637c017d857a0e480b4858b9bb80f2
Ian Blake <ian@ianbl8.com>
2023-12-31 22:50:08 +0000

    Reimplement email/password login through Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("kotlin-parcelize")
     id("kotlin-kapt")
     id("androidx.navigation.safeargs.kotlin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -52,6 +53,7 @@ dependencies {
     implementation("com.github.ajalt:timberkt:1.5.1")
     implementation("com.squareup.picasso:picasso:2.71828")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/ianbl8/mymusic/utils/Helpers.kt
@@ -0,0 +1,28 @@
+package com.ianbl8.mymusic.utils
+
+import android.app.AlertDialog
+import androidx.fragment.app.FragmentActivity
+import com.ianbl8.mymusic.R
+
+fun createLoader(activity: FragmentActivity) : AlertDialog {
+    val loaderBuilder = AlertDialog.Builder(activity)
+        .setCancelable(true) // 'false' if you want user to wait
+        .setView(R.layout.loading)
+    var loader = loaderBuilder.create()
+    loader.setTitle(R.string.app_name)
+    loader.setIcon(R.mipmap.ic_launcher_round)
+
+    return loader
+}
+
+fun showLoader(loader: AlertDialog, message: String) {
+    if (!loader.isShowing) {
+        loader.setTitle(message)
+        loader.show()
+    }
+}
+
+fun hideLoader(loader: AlertDialog) {
+    if (loader.isShowing)
+        loader.dismiss()
+}
